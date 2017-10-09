1d975fc3afe320281a395df0d000fc8feb7c7b40
Andrey Semochkin <admin@deepweb.spb.ru>
2017-10-09 21:26:31 +0300

    Update autotls.go

--- autotls.go
+++ autotls.go
@@ -11,13 +11,19 @@ import (
 func Run(r http.Handler, domain ...string) error {
 	return http.Serve(autocert.NewListener(domain...), r)
 }
+type DevNull struct{}
 
+func (DevNull) Write(p []byte) (int, error) {
+	return len(p), nil
+}
 // RunWithManager support custom autocert manager
 func RunWithManager(r http.Handler, m *autocert.Manager) error {
+	l := log.New(new(DevNull), "", 0)
 	s := &http.Server{
 		Addr:      ":https",
 		TLSConfig: &tls.Config{GetCertificate: m.GetCertificate},
 		Handler:   r,
+		ErrorLog: l,
 	}
 	s.SetKeepAlivesEnabled(false)
 	return s.ListenAndServeTLS("", "")
